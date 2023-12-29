374c56fc9177ebec92db3acef9d4bfa04a3337e8
behzadyazdani <153116399+behzadya@users.noreply.github.com>
2023-12-29 12:53:59 +0330

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main() {
+    float sum=0;
+    int i,a[10];
+    for ( i = 0 ; i<=9  ; i ++)
+    {
+        std::cin>>a[i];
+        sum=sum+a[i];
+    }
+      std::cout<<"sum :  "<<sum<<std::endl;
+    std::cout<<"average :  "<<sum/10 ;
+    }
+//مشکل اعشار میانگین رو با فلوت کردن سام حل کردم
+// کد وارد شده در جزوه سام رو اینتیجر قرار داده بود
+
+
+
+
+
+
+
+
